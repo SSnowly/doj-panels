57de5a7573e42a1bbb3a6aec5af0c6d3d0f01f8c
Snowly <visionhyronic@gmail.com>
2024-10-15 19:44:41 +0200

    e

--- web/assets/js/setting-demo.js
+++ web/assets/js/setting-demo.js
@@ -72,9 +72,9 @@ $(".changeBackgroundColor").on("click", function () {
 function customCheckColor() {
   var logoHeader = $(".logo-header").attr("data-background-color");
   if (logoHeader !== "white") {
-    $(".logo-header .navbar-brand").attr("src", "assets/img/kaiadmin/logo_light.svg");
+    $(".logo-header .navbar-brand").attr("src", "assets/img/logo2.png");
   } else {
-    $(".logo-header .navbar-brand").attr("src", "assets/img/kaiadmin/logo_dark.svg");
+    $(".logo-header .navbar-brand").attr("src", "assets/img/logo3.png");
   }
 }
 
